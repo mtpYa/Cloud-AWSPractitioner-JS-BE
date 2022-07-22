59c30f4af4d411773cd7278074fa6ec64a067c15
Taras_Melnyk <19mtp84@gmail.com>
2022-07-22 16:53:53 +0300

    Add content type for lambda

--- product-service/get-product-list-handler.js
+++ product-service/get-product-list-handler.js
@@ -3,6 +3,9 @@ const products = require('./data.json');
 module.exports.getProductsList = async (event) => {
   return {
     statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify({
       status: 'success',
       data: products.data
